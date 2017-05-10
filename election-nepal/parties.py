9c4498365faa2a3641091cd7a5a2e666725d25e5
Clifton McIntosh <cliff.mcintosh@gmail.com>
2017-05-09 21:23:52 -0500

    Update method and variable names

--- election-nepal/parties.py
+++ election-nepal/parties.py
@@ -10,7 +10,7 @@ from shared import geoidmappings
 Parties = namedtuple('Parties', 'place, parties')
 
 
-def voter_totals(parties_tuple):
+def political_parties(parties_tuple):
     if parties_tuple.place == 'Total':
         geo_level = 'country'
         geo_code = 'NP'
@@ -30,11 +30,11 @@ def convert_csv(inputfile, outputfile):
     with open(inputfile, 'r') as data, open(outputfile, 'w') as csv_out:
         reader = csv.reader(data)
         csv_rows = [row for row in reader][1:]
-        polling_place_data = [voter_totals(Parties(*row)) for row in csv_rows]
+        parties_data = [political_parties(Parties(*row)) for row in csv_rows]
         csv_keys = ['geo_level', 'geo_code', 'registered political parties']
         writer = csv.writer(csv_out)
         writer.writerow(csv_keys)
-        for row in polling_place_data:
+        for row in parties_data:
             writer.writerow([row['geo_level'],
                             row['geo_code'],
                             row['registered_parties']])
