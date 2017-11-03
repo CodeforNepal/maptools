a414f6006cfe3674a609c62f318afb72d3daf7c7
Bezzy1999 <Bezzy1999@users.noreply.github.com>
2017-11-03 08:11:19 +0000

    Updated check for missing districts

--- agriculture/meat.py
+++ agriculture/meat.py
@@ -1,111 +1,10 @@
 import csv
 import getopt
-
 import sys
 from collections import namedtuple
 
 from shared import geoidmappings
 
-'''
-names_to_geo_ids = {
-    'Aachham': '68',
-    'Achham': '68',
-    'Arghakhanchi': '46',
-    'Baglung': '51',
-    'Baitadi': '73',
-    'Bajhang': '69',
-    'Bajura': '67',
-    'Banke': '65',
-    'Bara': '32',
-    'Bardiya': '66',
-    'Bhaktapur': '25',
-    'Bhojpur': '06',
-    'Chitwa': '35',
-    'Chitwan': '35',
-    'Chitawan': '35',
-    'Dadeldhura': '74',
-    'Dailekha': '63',
-    'Dailekh': '63',
-    'Dang': '60',
-    'Darchaula': '72',
-    'Darchula': '72',
-    'Dhading': '30',
-    'Dhankuta': '07',
-    'Dhanusha': '20',
-    'Dhanusa': '20',
-    'Dolakha': '17',
-    'Dolpa': '52',
-    'Doti': '70',
-    'Gorkha': '36',
-    'Gulmi': '45',
-    'Humla': '56',
-    'Illam': '03',
-    'Ilam': '03',
-    'Jajarkot': '62',
-    'Jhapa': '04',
-    'Jumla': '54',
-    'Kailali': '71',
-    'Kalikot': '55',
-    'Kanchanpur': '75',
-    'Kapilbastu': '47',
-    'Kapilavastu': '47',
-    'Kapilvastu': '47',
-    'Kaski': '40',
-    'Kathmandu': '27',
-    'Kavrepalanchowk': '24',
-    'Kavrepalanchok': '24',
-    'Kavre': '24',
-    'Khotang': '13',
-    'Lalitpur': '26',
-    'Lamjung': '37',
-    'Mahottari': '21',
-    'Makawanpur': '34',
-    'Makwanpur': '34',
-    'Manang': '39',
-    'Morang': '09',
-    'Mugu': '53',
-    'Mustang': '48',
-    'Myagdi': '49',
-    'Nawalparasi': '42',
-    'Nawalparashi': '42',
-    'Nuwakot': '29',
-    'Okhaldhunga': '12',
-    'OKhaldhunga': '12',
-    'Palpa': '43',
-    'Panchthar': '02',
-    'Parbat': '50',
-    'Parsa': '33',
-    'Pyuthan': '59',
-    'Ramechap': '18',
-    'Ramechhap': '18',
-    'Rasuwa': '28',
-    'Rautahat': '31',
-    'Rolpa': '58',
-    'Rukum': '57',
-    'Rupandehi': '44',
-    'Salyan': '61',
-    'Sankhuwasabha': '05',
-    'Saptari': '15',
-    'Sarlahi': '22',
-    'Sindhuli': '19',
-    'Sindhupalchowk': '23',
-    'Sindhupalchok': '23',
-    'Siraha': '16',
-    'Solukhumbu': '11',
-    'Sunsari': '10',
-    'Surkhet': '64',
-    'Syangja': '41',
-    'Tanahun': '38',
-    'Tanahu': '38',
-    'Tahanun': '38',
-    'Taplejung': '01',
-    'Tehrathum': '08',
-    'Terhathum': '08',
-    'Terathum': '08',
-    'Udayapur': '14',
-    'Udaypur': '14'
-}
-'''
 
 meat_types = [
     'buff', 
@@ -146,8 +45,11 @@ def national_totals(district_rows):
     districts_with_data = set([row['geo_code'] for row 
         in district_rows])
 
-    print(all_districts - districts_with_data)
-    #return list(all_districts - districts_with_data) + [{
+    if len(all_districts - districts_with_data):
+        print('Districts without data:{0}'.format(
+            all_districts - districts_with_data)
+        )
+
     return [{
             'geo_level': 'country',
             'geo_code': 'NP',
